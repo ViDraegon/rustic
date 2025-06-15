fdaf798d8a87c60ec015a5f5c6989af2442988fc
ViDraegon <viliotdraegon@gmail.com>
2025-06-15 07:02:41 -0400

    init commit
    Starting point

---
+++ Rustic/Code/Block/BlockLargeRusticGenericContainer.cs
@@ -0,0 +1,18 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
+
+namespace Rustic.Code.Block
+{
+    public class BlockSmallRusticGenericContainer : BlockGenericTypedContainer
+    {
+
+    }
+
+    public class BlockLargeRusticGenericContainer : BlockGenericTypedContainerTrunk
+    {
+
+    }
+ 
+}
---
+++ Rustic/RusticModSystem.cs
@@ -0,0 +1,22 @@
+﻿using Rustic.Code.Block;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace Rustic
+{
+    public class RusticModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            // Block classes
+            api.RegisterBlockClass("Rustic.BlockLargeRusticGenericContainer", typeof(BlockLargeRusticGenericContainer));
+            api.RegisterBlockClass("Rustic.BlockSmallRusticGenericContainer", typeof(BlockSmallRusticGenericContainer));
+        }
+
+    }
+}
